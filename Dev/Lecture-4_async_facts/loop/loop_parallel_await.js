764d71ed4b532df0b28ca9c0e9db364fe1c6584d
Aditya Wasan <adityawasan55@gmail.com>
2020-04-28 17:00:13 +0530

    Update
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- Dev/Lecture-4_async_facts/loop/loop_parallel_await.js
+++ Dev/Lecture-4_async_facts/loop/loop_parallel_await.js
@@ -7,6 +7,6 @@ async function readFile(promise) {
 
 let files = ["../f1.txt", "../f2.txt", "../f3.txt", "../f4.txt", "../f5.txt"];
 
-for (let i = 0; i < files.length(); i++) {
+for (let i = 0; i < files.length; i++) {
     readFile(fs.promises.readFile(files[i]));
 }
