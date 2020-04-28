6fe7684c11b714d3ef7801d4a2d72a295d686158
Aditya Wasan <adityawasan55@gmail.com>
2020-04-28 16:59:04 +0530

    Update
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- Dev/Lecture-4_async_facts/loop/loop_serial_await.js
+++ Dev/Lecture-4_async_facts/loop/loop_serial_await.js
@@ -1,4 +1,11 @@
 let fs = require("fs");
-
 let files = ["../f1.txt", "../f2.txt", "../f3.txt", "../f4.txt", "../f5.txt"];
 
+async function myFun() {
+    for (let i = 0; i < files.length; i++) {
+    let a = await fs.promises.readFile(files[i]);
+    console.log(a + "")
+    }
+}
+
+myFun();
